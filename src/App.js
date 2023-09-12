c0af87ead13e95463b7dffdb630b0ef11cadfa3b
Maggos-sketch <maxbel2001@inbox.ru>
2023-09-12 21:53:00 +0300

    end first part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import reactLogo from './reactLogo.png';
 
 import React from "react";
 
 function App() {
   return (
     <div className="App">
-      
+      <img src={reactLogo} width={120} height={120}/>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </div>
   );
 }
