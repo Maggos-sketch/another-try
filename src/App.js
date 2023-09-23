d752ecc95d5c6955c8ce4a8ccb849d627fc43d6e
Maggos-sketch <maxbel2001@inbox.ru>
2023-09-23 17:56:41 +0300

    Make skelet

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import reactLogo from './reactLogo.png';
+//import reactLogo from './reactLogo.png';
 
 import React from "react";
+import Navbar from "./components/navbar";
+import Main from "./components/main";
 
 function App() {
   return (
-    <div className="App">
-      <img src={reactLogo} width={120} height={120}/>
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+    <div>
+      <Navbar />
+      <Main />
     </div>
   );
 }
