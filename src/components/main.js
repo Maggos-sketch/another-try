d39ac15612924cdfdf346bbe3528886a21f9a450
Maggos-sketch <maxbel2001@inbox.ru>
2023-09-23 19:09:20 +0300

    Make first project

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,21 @@
+import iconMain from "../iconMain.png";
 import React from "react";
 
 export default function Main(){
     return(
-        <h1>Main here!</h1>
+        <main className="main_container">
+            <h1 className="main_container__title main_set">Fun facts about React</h1>
+            <div className="list_and_image">
+                <ul className="list_ul">
+                    <li className="main_set li_style">Was first released in 2013</li>
+                    <li className="main_set li_style">Was originally created by Jordan Walke</li>
+                    <li className="main_set li_style">Has well over 100K stars on GitHub</li>
+                    <li className="main_set li_style">Is maintained by Facebook</li>
+                    <li className="main_set li_style">Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+                <img src={iconMain}></img>
+            </div>
+
+        </main>
     )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,14 @@
+import reactjsIcon from "../reactjsIcon.svg";
 import React from "react";
 
 export default function Navbar(){
     return(
-        <h1>Navbar here!</h1>
+        <header className="header_block">
+            <div className="header_block_logo">
+                <img src={reactjsIcon} className="header_block_logo__img"></img>
+                <p className="header_block_logo__title">ReactFacts</p>
+            </div>
+            <p className="header_block__course">React Course - Project 1</p>
+        </header>
     )
 }
